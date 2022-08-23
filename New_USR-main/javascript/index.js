b2962ed6d8d1df480b0f76ec09fda745a346fb37
Superhank718 <107980936+Superhank718@users.noreply.github.com>
2022-08-24 00:13:12 +0800

    Update index.js

--- New_USR-main/javascript/index.js
+++ New_USR-main/javascript/index.js
@@ -52,7 +52,7 @@ function gridStyle (n, wn, hn) {
   let ww = parseFloat($('.wrap').css('width')) 
   let wh = parseFloat($('.wrap').css('height')) 
   let gw = (ww - gm * (wn - 1)) / wn / 1.1
-  let gh = gw
+  let gh = (wh - gm * (hn - 1)) / hn * 0.33
   let gl = (gw + gm) * (n % wn)
   let gt = (gh + gm) * Math.floor(n / wn)
   let style = {
